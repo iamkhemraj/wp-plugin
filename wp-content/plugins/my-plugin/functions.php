74a9ad4bf61058b89cdbb5dac5e019fd12aa24dc
Khemraj <khemraj@rwit.io>
2024-02-07 14:51:15 +0530

    include jquery cdn

--- wp-content/plugins/my-plugin/functions.php
+++ wp-content/plugins/my-plugin/functions.php
@@ -1,9 +1,12 @@
 <?php
-  echo 'levelsss';
- function wpse_load_plugin_css() {
 
-  wp_enqueue_style( 'style-css', plugins_url('/admin-dashboard/css/style.css', __FILE__) );
-  wp_enqueue_style( 'bootstrap.min', plugins_url('/admin-dashboard/css/bootstrap.min.css', __FILE__) );
-  wp_enqueue_script( 'jquery', plugins_url('/admin-dashboard/js/main.js', __FILE__ ) );
-}
-add_action( 'wp_enqueue_scripts', 'wpse_load_plugin_css' );
+  require_once("libs/helper.php");
+  //require_once("libs/register-post.php");
+  // Function to enqueue scripts
+  function your_namespace() {
+    wp_register_script( 'main-js', plugins_url('/my-plugin/admin-dashboard/js/main.js'));
+    wp_enqueue_script('main-js');
+  }
+
+  add_action( 'admin_init','your_namespace');
+?>
