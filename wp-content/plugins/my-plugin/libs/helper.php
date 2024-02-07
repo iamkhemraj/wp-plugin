64ae421dfce8834bd4c66d0e114965fa8147465e
Khemraj <khemraj@rwit.io>
2024-02-07 15:37:06 +0530

    customize code

--- wp-content/plugins/my-plugin/libs/helper.php
+++ wp-content/plugins/my-plugin/libs/helper.php
@@ -1,57 +1,62 @@
 <?php
 
-  $formData = isset($_POST["formData"]) ? $_POST["formData"] : '' ;
-  $data = json_decode($formData);
-  print_r($data);
-  
+// Retrieve the serialized form data
+$post_type  = isset($_POST["post"]) ? $_POST["post"] : '';
+$category   = isset($_POST["catname"]) ? $_POST["catname"] : '';
+$tag        = isset($_POST["tagname"]) ? $_POST["tagname"] : '';
 
-  if(isset( $formData) &&  !empty($formData)){
-    // class Custom_Post_Type_Plugin {
 
-    //   // Constructor to initialize the plugin.
-    //   public function __construct() {
-    //       add_action('init', array($this, 'register_custom_post_type'));
-    //   }
+if (!empty( $post_type  && $category &&  $tag)) { 
+    class Custom_Post_Type_Plugin {
+
+      // Constructor to initialize the plugin.
+      public function __construct() {
+          add_action('init', array($this, 'register_custom_post_type'));
+      }
     
-    //   // Method to register the custom post type.
-    //   public function register_custom_post_type() {
-    //       $labels = array(
-    //           'name'               => _x('movie', 'post type general name'),
-    //           'singular_name'      => _x('Production', 'post type singular name'),
-    //           'add_new'            => _x('Add New', 'Production'),
-    //           'add_new_item'       => __('Add New Production'),
-    //           'edit_item'          => __('Edit Production'),
-    //           'new_item'           => __('New Production'),
-    //           'all_items'          => __('All movie'),
-    //           'view_item'          => __('View Production'),
-    //           'search_items'       => __('Search movie'),
-    //           'not_found'          => __('No movie found'),
-    //           'not_found_in_trash' => __('No movie found in Trash'),
-    //           'menu_name'          => 'movie'
-    //       );
+      // Method to register the custom post type.
+      public function register_custom_post_type() {
+          $labels = array(
+              'name'               => _x('movie', 'post type general name'),
+              'singular_name'      => _x('Production', 'post type singular name'),
+              'add_new'            => _x('Add New', 'Production'),
+              'add_new_item'       => __('Add New Production'),
+              'edit_item'          => __('Edit Production'),
+              'new_item'           => __('New Production'),
+              'all_items'          => __('All movie'),
+              'view_item'          => __('View Production'),
+              'search_items'       => __('Search movie'),
+              'not_found'          => __('No movie found'),
+              'not_found_in_trash' => __('No movie found in Trash'),
+              'menu_name'          => 'movie'
+          );
     
-    //       $args = array(
-    //           'labels'             => $labels,
-    //           'public'             => true,
-    //           'publicly_queryable' => true,
-    //           'show_ui'            => true,
-    //           'show_in_menu'       => true,
-    //           'query_var'          => true,
-    //           'rewrite'            => array('slug' => 'production'),
-    //           'capability_type'    => 'post',
-    //           'has_archive'        => true,
-    //           'hierarchical'       => false,
-    //           'menu_position'      => 5,
-    //           'supports'           => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments')
-    //       );
+          $args = array(
+              'labels'             => $labels,
+              'public'             => true,
+              'publicly_queryable' => true,
+              'show_ui'            => true,
+              'show_in_menu'       => true,
+              'query_var'          => true,
+              'rewrite'            => array('slug' => 'production'),
+              'capability_type'    => 'post',
+              'has_archive'        => true,
+              'hierarchical'       => false,
+              'menu_position'      => 5,
+              'supports'           => array('title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments')
+          );
     
-    //       // Register the custom post type.
-    //       register_post_type('production', $args);
-    //   }
-    // }
+          // Register the custom post type.
+          register_post_type('production', $args);
+      }
+    }
     
-    // // Instantiate the class to activate the plugin.
-    // new Custom_Post_Type_Plugin();
-    
+    // Call class
+    new Custom_Post_Type_Plugin();
     
   }
+
+
+
+    
+  
