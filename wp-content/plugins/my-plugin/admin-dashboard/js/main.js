d43663f31ff8fa0d06f1342b9be31663dbf05396
Khemraj <khemraj@rwit.io>
2024-02-07 10:36:01 +0530

    Create togglevissiable function enables post type field

--- wp-content/plugins/my-plugin/admin-dashboard/js/main.js
+++ wp-content/plugins/my-plugin/admin-dashboard/js/main.js
@@ -1,1 +1,24 @@
-alert('work');
+jQuery(document).ready(function($){
+
+  // Toggle vissiable category and tags fields
+  function toggleCatnameVisibility(checked , property) {
+    if (checked) {
+      $(property).removeClass("d-none");
+    } else {
+      $(property).addClass("d-none");
+    }
+  }
+
+  $('#category').click(function(){
+    var isChecked = $(this).prop('checked');
+    var property  = '#catname';
+    toggleCatnameVisibility(isChecked , property);
+  });
+
+  $('#tags').click(function(){
+    var isChecked = $(this).prop('checked');
+    var property  = '#tagname';
+    toggleCatnameVisibility(isChecked , property);
+  });
+
+});
