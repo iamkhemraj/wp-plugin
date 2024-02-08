154110e70b47fdd68be9927212d2c530d5b6dc95
Khemraj <khemraj@rwit.io>
2024-02-08 16:29:24 +0530

    Modify code

--- wp-content/plugins/my-plugin/admin-dashboard/index.php
+++ wp-content/plugins/my-plugin/admin-dashboard/index.php
@@ -1,42 +1,40 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Custom post</title>
-</head>
-<body>
-  
-  <div class="container p-4">
-      <div class="post-title">
-        <h3>Register Post Type</h3>
-      </div>
-      <form id="form">
-        <div class="row">
-          <div class="col-md-6">
-            <div class="group-field">            
-              <input type="text " name="post" placeholder="Enter post type" class="form-control">
-              <label for="post">Post Type</label>
-            </div>  
-          </div>
-          <div class="col-md-6">
-            <div class="group-field">
-                <input type="checkbox" name="category" id="category">
-                <label for="category">Category</label><br>
-                <input type="text" name="catname" id="catname" placeholder="Enter category name" class="d-none"><br>
-                <input type="checkbox" name="tags" id="tags">
-                <label for="tags">Tags</label><br>
-                <input type="text" name="tagname" id="tagname" placeholder="Enter tags name" class="d-none">
-              </div>
-          </div>
-        </div><br>
-        <input type="submit" value="Create post" class="btn bg-dark text-white">
-      </form>
-  </div>
 
-  <!-- script links cdn  -->
-  
+<head>
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Custom cpt</title>
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <form method="post">
+                    <div class="mb-3">
+                        <label for="post_type" class="form-label">Post Type:</label>
+                        <input type="text" id="post_type" name="post_type" class="form-control" required>
+                    </div>
+                    <div class="mb-3">
+                        <label for="category" class="form-label">Category:</label>
+                        <input type="text" id="category" name="category" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label for="tag" class="form-label">Tag:</label>
+                        <input type="text" id="tag" name="tag" class="form-control">
+                    </div>
+                    <button type="submit" name="create_post_type" class="btn btn-primary">Create Post Type</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <!-- script links cdn  -->
+
 </body>
+
 </html>
--- wp-content/plugins/my-plugin/my-plugin.php
+++ wp-content/plugins/my-plugin/my-plugin.php
@@ -55,32 +55,9 @@
     }
     add_action('init', 'create_cpt');
 
-    function display_cpt_form() {  // Dispaly cpt form  ?> 
-
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-
-        <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-6">
-                    <form method="post">
-                        <div class="mb-3">
-                            <label for="post_type" class="form-label">Post Type:</label>
-                            <input type="text" id="post_type" name="post_type" class="form-control" required>
-                        </div>
-                        <div class="mb-3">
-                            <label for="category" class="form-label">Category:</label>
-                            <input type="text" id="category" name="category" class="form-control">
-                        </div>
-                        <div class="mb-3">
-                            <label for="tag" class="form-label">Tag:</label>
-                            <input type="text" id="tag" name="tag" class="form-control">
-                        </div>
-                        <button type="submit" name="create_post_type" class="btn btn-primary">Create Post Type</button>
-                    </form>
-                </div>
-            </div>
-        </div> <?php
+    function display_cpt_form() {  // Dispaly cpt form 
+    
+        require_once("admin-dashboard/index.php"); // include cpt form
     
     } 
     function plugin_menu_page() {   // Register a custom menu page.
