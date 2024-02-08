eea60aac1091f8e40f9e3010d2bc7494d36d6c14
Khemraj <khemraj@rwit.io>
2024-02-08 17:50:23 +0530

    modify code

--- wp-content/plugins/my-plugin/my-plugin.php
+++ wp-content/plugins/my-plugin/my-plugin.php
@@ -37,21 +37,27 @@
         $get_cpt    = "SELECT * FROM $table_name WHERE id != 0";
         $cpt_data   = $wpdb->get_results($get_cpt);
         register_post_types($cpt_data);
-
+        // define variables to empty values  
+        $post_typeErr = $categoryErr = $tagErr =  $post_type = $category = $tag  = "" ;
         if(isset($_POST['create_post_type'])) {
 
             $post_type = isset($_POST['post_type']) ? sanitize_text_field($_POST['post_type']) : '';
             $category  = isset($_POST['category']) ? sanitize_text_field($_POST['category']) : '';
             $tag       = isset($_POST['tag']) ? sanitize_text_field($_POST['tag']) : '';
-            $wpdb->insert($table_name, array(  // Insert into database
-                'post_type' => $post_type,
-                'category'  => $category,
-                'tag'       => $tag
-            ));
+            if (!empty($post_type) && !empty($category) && !empty($tag)) {
+                $wpdb->insert($table_name, array(  // Insert into database
+                    'post_type' => $post_type,
+                    'category'  => $category,
+                    'tag'       => $tag
+                ));
+    
+                $get_cpt  = "SELECT * FROM $table_name WHERE id != 0";  // Select data from wp_cpt table
+                $cpt_data =  $wpdb->get_results($get_cpt);
+                register_post_types($cpt_data);
 
-            $get_cpt  = "SELECT * FROM $table_name WHERE id != 0";  // Select data from wp_cpt table
-            $cpt_data =  $wpdb->get_results($get_cpt);
-            register_post_types($cpt_data);
+            }
+        
+           
         }
     }
     add_action('init', 'create_cpt');
