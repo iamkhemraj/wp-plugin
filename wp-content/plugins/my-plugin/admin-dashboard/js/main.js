c8dbaa10c8f4c6ba5e6a191366b4044d5c09c4a7
Khemraj <khemraj@rwit.io>
2024-02-08 17:51:34 +0530

    add jquery validation

--- wp-content/plugins/my-plugin/admin-dashboard/js/main.js
+++ wp-content/plugins/my-plugin/admin-dashboard/js/main.js
@@ -1,7 +1,6 @@
 jQuery(document).ready(function($){
 
-  // Toggle visibility of category and tags fields
-  function toggleFieldVisibility(checked, property) {
+  function toggleFieldVisibility(checked, property) { // Toggle visibility of cpt fields
     if (checked) {
       $(property).removeClass("d-none");
     } else {
@@ -21,4 +20,27 @@ jQuery(document).ready(function($){
     toggleFieldVisibility(isChecked, property);
   });
 
+  $("#form").validate({  // jQuery cpt form validation
+    rules: {
+        post_type: {
+            required: true,
+            minlength: 2,
+            maxlength:15
+        },
+  
+    },
+    messages: {
+        post_type: {
+            required: "Please enter post type",
+            maxlength:"max length 15 digits",
+            minlength: "Your username must consist of at least 2 characters"
+        },
+      
+    }
+  });
+
 });
+
+
+
+
