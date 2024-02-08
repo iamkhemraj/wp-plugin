865d5ccbac9eaf5a775a9a33b46ac6d41c9ab9a4
Khemraj <khemraj@rwit.io>
2024-02-08 17:50:01 +0530

    add jquery valiadtion function

--- wp-content/plugins/my-plugin/functions.php
+++ wp-content/plugins/my-plugin/functions.php
@@ -5,7 +5,9 @@
   // Function to enqueue scripts
   function your_namespace() {
     wp_register_script( 'main-js', plugins_url('/my-plugin/admin-dashboard/js/main.js'));
+    wp_register_script( 'validate-min-js', "https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.20.0/jquery.validate.min.js");
     wp_enqueue_script('main-js');
+    wp_enqueue_script('validate-min-js');
   }
 
   add_action( 'admin_init','your_namespace');
