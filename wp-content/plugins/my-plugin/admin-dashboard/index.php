9c02a28ba108c5dc65fbe3a4a0a69718150f5670
Khemraj <khemraj@rwit.io>
2024-02-13 12:35:07 +0530

    Add title get all post type

--- wp-content/plugins/my-plugin/admin-dashboard/index.php
+++ wp-content/plugins/my-plugin/admin-dashboard/index.php
@@ -8,32 +8,37 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Custom post</title>
     <style>
-        
-        .post-type-section , .get_post_type {
+        .post-type-section {
             border: 1px solid #c3c4c7;
-            box-shadow: 0 1px 1px rgba(0,0,0,.04);
-            padding: 20px 0px  20px 20px;
-            width: 80%;
+            box-shadow: 0 1px 1px rgba(0, 0, 0, .04);
+            padding: 20px 0px 20px 20px;
+            width: 100%;
         }
+
         .post-title h1 {
             font-weight: 400;
             font-size: 23px;
         }
+
         input[type='text'] {
-            width: 50%;
+            width: 80%;
         }
-        .error{
-            color:red;
-        } 
-        #create-custom-post{
-            color:#fff !important;
+
+        .error {
+            color: red;
+        }
+
+        #create-custom-post {
+            color: #fff !important;
             background-color: #2271b1 !important;
         }
+
         .get_post_type {
             border: 1px solid #c3c4c7;
-            box-shadow: 0 1px 1px rgba(0,0,0,.04);
-            padding: 20px 0px  20px 20px;
-            width: 80%;
+            box-shadow: 0 1px 1px rgba(0, 0, 0, .04);
+            padding: 20px 0px 20px 20px;
+            width: 30%;
+            margin-top: 53px;
         }
     </style>
 </head>
@@ -41,63 +46,81 @@
 <body>
 
     <div class="container p-4">
-        <div class="post-type-section">
-       
-            <div class="post-title">
-                <h1>Register Post Type</h1>
-            </div>
-            <form id="form" method="POST">
-                <div class="row">
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <label for="post_type" class="form-label">Post Type:</label>
-                            <input type="text" name="post_type" id="post_type" class=" form-control  input-sm" size = "50px" placeholder="Enter post type"  maxlength="20" >
-                            <span class="error"> 
-                                <?= !empty($_SESSION['error_message']) ? $_SESSION['error_message'] : '' ; ?>
-                                <?= !empty($_SESSION['cpt_exists']) ? $_SESSION['cpt_exists'] : '' ; ?>
-                            </span> 
-                            
-                        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="post-type-section">
+                    <div class="post-title">
+                        <h1>Register Post Type</h1>
                     </div>
-                    <div class="col-md-6">
-                        <div class="form-group">
-                            <input type="checkbox" id="category_check">
-                            <label for="category_check" class="form-label">Category</label>
-                            <input type="text" name="category" id="category" class=" form-control input-sm  input-sm d-none" placeholder="Enter category name" maxlength="20">
-                            <span class="error d-block"> <?php if(  !empty($_SESSION['category_err'])){ echo  $_SESSION['category_err'];}?></span> 
-                           
-                            <input type="checkbox" id="tags_check">
-                            <label for="tags_check" class="form-label">Tags</label>
-                            <input type="text" name="tags" id="tags" class=" form-control  input-sm d-none" placeholder="Enter tags name">
-                            <span class="error d-block"> <?php if(  !empty($_SESSION['tag_err'])){ echo  $_SESSION['tag_err'];}?></span> 
-                        </div>
-                    </div>
-                </div><br>
-                <button type="submit" id="create-custom-post" class="btn" name="create_post_type">Create
-                    Post</button>
-            </form>
-        </div>
+                    <form id="form" method="POST">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label for="post_type" class="form-label">Post Type:</label>
+                                    <input type="text" name="post_type" id="post_type" class=" form-control  input-sm"
+                                        size="50px" placeholder="Enter post type" maxlength="20">
+                                    <span class="error">
+                                        <?= !empty($_SESSION['error_message']) ? $_SESSION['error_message'] : ''; ?>
+                                        <?= !empty($_SESSION['cpt_exists']) ? $_SESSION['cpt_exists'] : ''; ?>
+                                    </span>
 
+                                </div>
+                            </div>
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <input type="checkbox" id="category_check">
+                                    <label for="category_check" class="form-label">Category</label>
+                                    <input type="text" name="category" id="category"
+                                        class=" form-control input-sm  input-sm d-none"
+                                        placeholder="Enter category name" maxlength="20">
+                                    <span class="error d-block">
+                                        <?php if (!empty($_SESSION['category_err'])) {
+                                            echo $_SESSION['category_err'];
+                                        } ?>
+                                    </span>
+
+                                    <input type="checkbox" id="tags_check">
+                                    <label for="tags_check" class="form-label">Tags</label>
+                                    <input type="text" name="tags" id="tags" class=" form-control  input-sm d-none"
+                                        placeholder="Enter tags name">
+                                    <span class="error d-block">
+                                        <?php if (!empty($_SESSION['tag_err'])) {
+                                            echo $_SESSION['tag_err'];
+                                        } ?>
+                                    </span>
+                                </div>
+                            </div>
+                        </div><br>
+                        <button type="submit" id="create-custom-post" class="btn" name="create_post_type">Create
+                            Post</button>
+                    </form>
+                </div>
+            </div>
+        
+            <div class="col-md-12">
+                <!-- Show all custom post type -->
+                <div class="get_post_type">
+                    <div class="post-title">
+                        <h6>All Post Type</h6>
+                    </div> <?php
+                    if (isset($_SESSION['get_post_type']) && !empty($_SESSION['get_post_type'])) {
+                        $cpt_lists = $_SESSION['get_post_type'];
+                        foreach ($cpt_lists as $list) {
+                            $cpt_name = !empty($list->post_type) ? $list->post_type : ''; ?>
+                            <div class="post_type-list">
+                                <input type="checkbox" name="cpt-list" id="cpt-list">
+                                <?= $cpt_name ?>
+                            </div>
+                        <?php
+                        }
+
+                    } ?>
+                </div>
+            </div>
+            
         </div>
     </div>
-
-     <!-- Show all custom post type -->
-     <div class="get_post_type">
-            
-            <?php
-                if(isset( $_SESSION['get_post_type'])  && !empty($_SESSION['get_post_type'])){
-                    $cpt_lists = $_SESSION['get_post_type'];
-                    foreach ( $cpt_lists as  $list){
-                        $cpt_name = !empty($list->post_type) ? $list->post_type : ''; ?>
-                        <div class="post_type-list">
-                            <input type="checkbox" name="cpt-list" id="cpt-list">
-                            <?= $cpt_name ?>
-                        </div> <?php    
-                    }
-                    
-                }
-            ?>
-
+   
 </body>
 
 </html>
