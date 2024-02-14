578f53ef0b0ab1dabb6ff59476b8a23ae4228d63
Khemraj <khemraj@rwit.io>
2024-02-14 10:50:23 +0530

    Add register post type

--- wp-content/plugins/my-plugin/libs/helper.php
+++ wp-content/plugins/my-plugin/libs/helper.php
@@ -1,5 +1,20 @@
 <?php
-
-// Custom post type form validation here
-
-?>
+       
+    function register_post_types($cpt_data)    // Register Custom post type
+    {
+        if ($cpt_data) {
+            foreach ($cpt_data as $cpt) {
+                $post_type = $cpt->post_type;
+                register_post_type($post_type, array(   // Register custom post type
+                    'labels' => array(
+                        'name'         => $post_type,          
+                        'singular_name'=> $post_type
+                    ),
+                    'public'      => true,
+                    'has_archive' => true,
+                    'taxonomies'  => array('category', 'post_tag')
+                ));
+            }
+        }
+    }
+    
