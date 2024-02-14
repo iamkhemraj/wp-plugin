a1649b0f3f1fdbd14475ac848d35a5e75cdd1054
Khemraj <khemraj@rwit.io>
2024-02-14 10:11:29 +0530

    Change messages

--- wp-content/plugins/my-plugin/libs/unregister-cpt.php
+++ wp-content/plugins/my-plugin/libs/unregister-cpt.php
@@ -1,43 +1,17 @@
 <?php
 
-    // Register Custom post type
-    function register_post_types($cpt_data)
-    {
-        if ($cpt_data) {
-            foreach ($cpt_data as $cpt) {
-                $post_type = $cpt->post_type;
-
-                // Register custom post type
-                register_post_type($post_type, array(
-                    'labels' => array(
-                        'name' => $post_type,
-                        'singular_name' => $post_type
-                    ),
-                    'public' => true,
-                    'has_archive' => true,
-                    'taxonomies' => array('category', 'post_tag')
-                )
-                );
-            }
-        }
-    }
-
-
-    
     // Load WordPress environment
     require_once($_SERVER['DOCUMENT_ROOT'] . '/wp-load.php');
-
-    // Check if postName is sent via POST request
     if (isset($_POST['postName'])) {
         $postName = $_POST['postName'];
 
         // Check if the post type exists
         if (post_type_exists($postName)) {
-            // Unregister the post type
+
             unregister_post_type($postName);
-            echo 'success';
+            echo  $postName.' post type unregister successfuly!';
         } else {
-            echo 'error';
+            echo  $postName.' post type does not unregister!';
         }
     } else {
         echo 'error';
