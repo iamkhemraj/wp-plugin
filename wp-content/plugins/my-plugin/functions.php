20e1a84a54efef8f6a81fa306a02f5f7b4c3705a
Khemraj <khemraj@rwit.io>
2024-02-14 10:11:04 +0530

    Change enque position

--- wp-content/plugins/my-plugin/functions.php
+++ wp-content/plugins/my-plugin/functions.php
@@ -3,12 +3,12 @@
   require_once("libs/helper.php");
 
   function your_namespace() {
-    wp_register_style( 'style-css', plugins_url('/my-plugin/admin-dashboard/css/style.css'));
-    wp_register_script( 'main-js', plugins_url('/my-plugin/admin-dashboard/js/main.js'));
-    wp_register_script( 'validate-min-js', "https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.20.0/jquery.validate.min.js");
-    wp_enqueue_script('main-js');
-    wp_enqueue_script('validate-min-js');
+    wp_register_style('style-css', plugins_url('/my-plugin/admin-dashboard/css/style.css'));
     wp_enqueue_style('style-css');
+    wp_register_script('main-js', plugins_url('/my-plugin/admin-dashboard/js/main.js'));
+    wp_enqueue_script('main-js');
+    wp_register_script('validate-min-js', "https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.20.0/jquery.validate.min.js");
+    wp_enqueue_script('validate-min-js');
   }
 
   add_action( 'admin_init','your_namespace');
