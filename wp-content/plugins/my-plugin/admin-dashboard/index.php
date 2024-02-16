7c635afd20b86623cccf8de6e9f4e9c0b50273c9
Khemraj <khemraj@rwit.io>
2024-02-16 15:17:43 +0530

    add dlete post qeury

--- wp-content/plugins/my-plugin/admin-dashboard/index.php
+++ wp-content/plugins/my-plugin/admin-dashboard/index.php
@@ -29,58 +29,73 @@ if (!defined('ABSPATH')) {
                                 <div class="col-md-6">
                                     <div class="form-group">
                                         <label for="post_type" class="form-label">Post Type:</label>
-                                        <input type="text" name="post_type" id="post_type" class=" form-control  input-sm"
-                                            size="50px" placeholder="Enter post type" maxlength="20"  value="<?= isset($_POST['post_type']) ? $_POST['post_type'] : '' ?>">
+                                        <input type="text" name="post_type" id="post_type"
+                                            class=" form-control  input-sm" size="50px" placeholder="Enter post type"
+                                            maxlength="20"
+                                            value="<?= isset($_POST['post_type']) ? $_POST['post_type'] : '' ?>">
                                         <span class="error">
-                                            <?= !empty($_SESSION['error_message']) ? $_SESSION['error_message'] : ''; ?>
+                                            <?= !empty($_SESSION['post_type_err']) ? $_SESSION['post_type_err'] : ''; ?>
                                             <?= !empty($_SESSION['cpt_exists']) ? $_SESSION['cpt_exists'] : ''; ?>
                                         </span>
                                     </div>
                                 </div>
                                 <div class="col-md-6">
                                     <div class="form-group">
-                                        <input type="checkbox" id="category_check" name = "category_check">
+                                        <input type="checkbox" id="category_check" name="category_check">
                                         <label for="category_check" class="form-label">Category</label>
-                                        <input type="text" name="category" id="category" class=" form-control input-sm  input-sm d-none" placeholder="Enter category name" maxlength="20" value="<?= isset($_POST['category']) ? $_POST['post_type'] : '' ?>" >
-                                        <span class="error d-block"><?= !empty($_SESSION['category_err']) ? $_SESSION['category_err'] : ''; ?></span>
+                                        <input type="text" name="category" id="category"
+                                            class=" form-control input-sm  input-sm d-none"
+                                            placeholder="Enter category name" maxlength="20"
+                                            value="<?= isset($_POST['category']) ? $_POST['category'] : '' ?>">
+                                        <span class="error d-block">
+                                            <?= !empty($_SESSION['category_err']) ? $_SESSION['category_err'] : ''; ?>
+                                        </span>
                                         <input type="checkbox" id="tags_check" name="tags_check">
                                         <label for="tags_check" class="form-label">Tags</label>
-                                        <input type="text" name="tags" id="tags" class=" form-control  input-sm d-none" placeholder="Enter tags name" value="<?= isset($_POST['tags']) ? $_POST['tags'] : '' ?>">
-                                        <span class="error d-block"><?= !empty($_SESSION['tag_err']) ? $_SESSION['tag_err'] : ''; ?></span>
-                                   </div>
+                                        <input type="text" name="tags" id="tags" class=" form-control  input-sm d-none"
+                                            placeholder="Enter tags name"
+                                            value="<?= isset($_POST['tags']) ? $_POST['tags'] : '' ?>">
+                                        <span class="error d-block">
+                                            <?= !empty($_SESSION['tag_err']) ? $_SESSION['tag_err'] : ''; ?>
+                                        </span>
+                                    </div>
                                 </div>
                             </div><br>
-                            <button type="submit" id="create-custom-post" class="btn" name="create_post_type">Create post</button>
+                            <button type="submit" id="create-custom-post" class="btn" name="create_post_type">Create
+                                post</button>
+                            <?= isset($_SESSION['dataInsert']) ? ' <span class="alert "> ' . $_SESSION["dataInsert"] . '</span>' : ''; ?>
                         </form>
                     </div>
                 </div>
-    
+
                 <div class="col-md-12">
                     <!-- Show all custom post type -->
                     <?php
-                        if (isset($_SESSION['get_post_type']) && !empty($_SESSION['get_post_type'])) { ?>
-                            <div class="get_post_type">
-                                <div class="post-title">
-                                    <h6>All Post Type</h6>
-                                </div>
-                                <div class="post_type-list "> <?php
+                    if (isset($_SESSION['get_post_type']) && !empty($_SESSION['get_post_type'])) { ?>
+                        <div class="get_post_type">
+                            <div class="post-title">
+                                <h6>All Post Type</h6>
+                            </div>
+                            <div class="post_type-list ">
+                                <?php
                                 $cpt_lists = $_SESSION['get_post_type'];
                                 foreach ($cpt_lists as $list) {
                                     $cpt_name = !empty($list->post_type) ? $list->post_type : '';
                                     $cpt_ID = !empty($list->id) ? $list->id : '';
                                     ($list->is_activate == true) ? $checked = 'checked' : $checked = '';
-                                    echo !empty($cpt_name) ? "<input type='checkbox' name='cpt-list' class='cpt-list' value='$cpt_ID' $checked> $cpt_name " : '';
-                                    // echo !empty($cpt_name) ?
-                                    // "<form id='deleteForm' method="post">
-                                    //     <input type='hidden' name='del' value='" . $cpt_ID . "'>
-                                    //     <input type='button' class='text-danger' value =' Delete'>
-                                    // </form><br>"
-                                    // : '';
-                            
-                                } ?> 
-                                </div>
-                            </div> <?php
-                        }
+                                    echo !empty($cpt_name) ? "<input type='checkbox' name='cpt-list' class='cpt-list' value='$cpt_ID' $checked> $cpt_name <br> " : '';
+                                    echo !empty($cpt_name) ?
+                                        "<form class='deleteForm' method='POST' action =''>
+                                        <input type='hidden' name='del' class='delete_cpt' value='" . $cpt_ID . "'>
+                                        <input type='button'class='deleteButton text-danger' value =' Delete'> <br>
+                                    </form><br>"
+                                        : '';
+
+                                } ?>
+                            </div>
+                        </div>
+                        <?php
+                    }
                     ?>
                 </div>
             </div>
@@ -88,82 +103,46 @@ if (!defined('ABSPATH')) {
     </div>
 </body>
 
-    <script>
-        jQuery(document).ready(function($){
-
-            $('.cpt-list').change(function () {
-                const postID = $(this).val().trim(); // Get post type value
-                const path = '<?php echo plugin_dir_url(__DIR__) . '/libs/unregister-cpt.php' ?>'; // Get the path to 
-                $.ajax({
-                    url: path,
-                    type: 'POST',
-                    data: {postID: postID},
-                    success: function(response) {
-                        if (response == true) {
-                            alert(response);
-                            setTimeout(function() {
-                                location.reload();
-                            }, 1000); // Reload page after 1 second (1000 milliseconds)
-                        } else {
-                            alert(response);
-                            setTimeout(function() {
-                                location.reload();
-                            }, 1000); // Refresh page after this message
-                        }
-                    }
-                });
-            });
-        });
-    </script>
-
 </html>
 
-<!-- <form id="deleteForm" method="post">
-    <input type="hidden" name="del" id="delPostID">
-    <button type="button" id="deleteButton">Delete Post</button>
-</form> -->
-
 <script>
-// jQuery(document).ready(function($) {
-//     $('#deleteButton').click(function() {
-//         var delPostID = $('#delPostID').val(); // Get the post ID from the hidden input field
-        
-//         // AJAX request
-//         $.ajax({
-//             url: '<?php //echo admin_url('admin-ajax.php');    ?>',
-//             type: 'POST',
-//             data: {
-//                 action: 'delete_custom_post', // AJAX action hook
-//                 del: delPostID // Post ID to be deleted
-//             },
-//             success: function(response) {
-//                 console.log(response); // Log the response to the console
-//                 // Handle success response, e.g., show a success message
-//             },
-//             error: function(xhr, status, error) {
-//                 console.error(xhr.responseText); // Log the error response to the console
-//                 // Handle error response, e.g., show an error message
-//             }
-//         });
-//     });
-// });
-</script>
+jQuery(document).ready(function($){
 
-<?php
-// add_action('wp_ajax_delete_custom_post', 'delete_custom_post_callback');
-// function delete_custom_post_callback() {
-//     if(isset($_POST['del'])) {
-//         $del_postID = isset($_POST['del']) ? intval($_POST['del']) : 0; // Sanitize and validate the post ID for deletion
+  $('.cpt-list').change(function () {
+    const postID = $(this).val().trim(); // Get post type value
+    const path   = '<?php echo plugin_dir_url(__DIR__) . '/libs/unregister-cpt.php' ?>'; // Get the path to 
+    $.ajax({
+      url: path,
+      type: 'POST',
+      data: {postID: postID},
+      success: function(response) {
+          if (response == true) {
+            alert(response);
+            setTimeout(function() {
+              location.reload();
+            }, 1000); // Reload page after 1 second (1000 milliseconds)
+          } else {
+            alert(response);
+            setTimeout(function() {
+              location.reload();
+            }, 1000); // Refresh page after this message
+          }
+      }
+  });
+});
 
-//         // Your delete query
-//         global $wpdb;
-//         $table_name = $wpdb->prefix . 'cpt';
-//         $wpdb->delete($table_name, array('id' => $del_postID), array('%d'));
-
-//         echo 'Post with ID ' . $del_postID . ' deleted successfully.';
-//     }
-//     wp_die(); // Always include this line to end AJAX requests
-// }
-
-?>
-
+$('.deleteButton').click(function(e){ // Delete custom post type Query here
+  $deleteCpt =  $(this).prev('.delete_cpt').val();
+  const path = '<?php echo plugin_dir_url(__DIR__) . '/libs/unregister-cpt.php' ?>'; 
+  $.ajax({
+      url: path,
+      type: 'post',
+      data:{ deleteCpt: $deleteCpt },
+      success : function(response){
+        console.log(response);
+      }
+  });
+});
+});
+    
+</script> 
