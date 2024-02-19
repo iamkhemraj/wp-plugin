8f0a6b2b037a3f2ca1864f226bfcde693d1a5309
khemraj <khemraj@rwit.io>
2024-02-19 17:12:41 +0530

    WORKING

--- wp-content/plugins/my-plugin/admin-dashboard/js/main.js
+++ wp-content/plugins/my-plugin/admin-dashboard/js/main.js
@@ -1,30 +1,46 @@
-jQuery(document).ready(function ($) {
-
-  function toggleFieldVisibility(ischecked, property, check) { // Toggle visibility of cpt fields
-    if (ischecked) {
-      $(check).prop('checked', true); // Ensure checkbox is checked
-      $(property).removeClass("d-none");
-    } else {
-      $(property).addClass("d-none");
-    }
+jQuery(document).ready(function($) {
+  // Function to toggle visibility of cpt fields
+  function toggleFieldVisibility(ischecked, property, check) {
+      if (ischecked) {
+          $(check).prop('checked', true); // Ensure checkbox is checked
+          $(property).removeClass("d-none");
+      } else {
+          $(property).addClass("d-none");
+      }
   }
 
-  $('#category_check').change(function () { // Query for category checkbox
-     var check     = $(this);
-     var isChecked = $(this).prop('checked');
-     var property  = '#category';
-     toggleFieldVisibility(isChecked, property, check);
-  }).change(); 
-  $('#tags_check').change(function () { // Query for tags checkbox
-     var check     = $(this);
-     var isChecked = $(this).prop('checked');
-     var property  = '#tags';
-     toggleFieldVisibility(isChecked, property, check);
-  }).change(); 
+  // Function to save checkbox state in local storage
+  function saveCheckboxState(checkboxId, isChecked) {
+      localStorage.setItem(checkboxId, isChecked);
+  }
 
-  //After registration alert close auto
-  setTimeout(function () {  
-    $('#alert').alert('close');  
-  }, 5000); 
+  // Function to get checkbox state from local storage
+  function getCheckboxState(checkboxId) {
+      return localStorage.getItem(checkboxId) === "true";
+  }
 
+  // Handle category checkbox
+  var categoryCheckbox = $('#category_check');
+  categoryCheckbox.prop('checked', getCheckboxState('category_check')); // Set initial state from local storage
+  categoryCheckbox.change(function() {
+      var isChecked = $(this).prop('checked');
+      saveCheckboxState('category_check', isChecked);
+      var property = '#category';
+      toggleFieldVisibility(isChecked, property, this);
+  }).change();
+
+  // Handle tags checkbox
+  var tagsCheckbox = $('#tags_check');
+  tagsCheckbox.prop('checked', getCheckboxState('tags_check')); // Set initial state from local storage
+  tagsCheckbox.change(function() {
+      var isChecked = $(this).prop('checked');
+      saveCheckboxState('tags_check', isChecked);
+      var property = '#tags';
+      toggleFieldVisibility(isChecked, property, this);
+  }).change();
+
+  // Automatically close alert after 5 seconds
+  setTimeout(function() {
+      $('#alert').alert('close');
+  }, 5000);
 });
