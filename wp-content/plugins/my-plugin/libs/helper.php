d3edcb8501af6630439ba05a5eaf1c9e765e4ef9
khemraj <khemraj@rwit.io>
2024-02-20 15:05:34 +0530

    code formatting

--- wp-content/plugins/my-plugin/libs/helper.php
+++ wp-content/plugins/my-plugin/libs/helper.php
@@ -3,8 +3,8 @@
         exit; // Exit if accessed directly
     }
 
-    function register_post_types($cpt_data) {
-        if (isset($cpt_data)) {
+    function register_post_types($cpt_data) { //Post type register here
+        if (isset($cpt_data) && !empty($cpt_data)) {
             foreach ($cpt_data as $cpt) {
                 if ($cpt->is_activate == true) {
                     $post_type = isset($cpt->post_type ) ? $cpt->post_type: '' ;
@@ -13,15 +13,14 @@
                     $term_id   = isset($cpt->id ) ? $cpt->id: '' ;             
                     $category  = isset($cpt->category )? ucwords($cpt->category) : '' ; 
                     $taxonomy  = str_replace(' ', '_', strtolower($category)); 
-                    $tax_slug  = !empty($taxonomy) ? $taxonomy :'category'; // Replace spaces with underscores
+                    $tax_slug  = !empty($taxonomy) ? $taxonomy :'category'; //Replace spaces with underscores
 
                     $rewrite_slug = array(
-                        'slug' => $slug,
+                        'slug'       => $slug,
                         'with_front' => false // This line removes the taxonomy slug from the URL
                     );
 
-                    // Register custom post type
-                    register_post_type($slug, array(
+                    register_post_type($slug, array(  //Register custom post type
                         'labels'        => array(
                             'name'          => $post_type,
                             'singular_name' => $post_type
@@ -29,18 +28,16 @@
                         'public'      => true,
                         'has_archive' => true,
                         'rewrite'     => $rewrite_slug,
-                        'taxonomies'  => array('post_tag') // Use the new taxonomy name here
+                        'taxonomies'  => array('post_tag') //Use the new taxonomy name here
                     ));
 
-                    // Register custom taxonomy for categories 
-
-                    register_taxonomy( $tax_slug , $slug, array(
+                    register_taxonomy( $tax_slug , $slug, array(  //Register custom taxonomy for categories 
                         "hierarchical"      => true,
                         "labels"            => array(
                             "name"          => "Categories",
                             "singular_name" => "Custom Category",
                         ),
-                        "rewrite"           => array("slug" => $slug), // Rewrite slug  
+                        "rewrite"           => array("slug" => $slug), //Rewrite slug  
                         "public"            => true,
                         "show_ui"           => true,
                         "show_in_rest"      => true,
@@ -55,11 +52,11 @@
                         "show_in_nav_menus" => true,
                     ));
 
-                    if (isset($category) && !empty($category)) {  // Insert category       
+                    if (isset($category) && !empty($category)) {  //Insert category       
                         $term = wp_insert_term($category, $tax_slug);   
                     }
 
-                    if (isset($tag_data) && !empty($tag_data)) { // Insert tags
+                    if (isset($tag_data) && !empty($tag_data)) { //Insert tags
                         $term = wp_insert_term($tag_data, 'post_tag');     
                     }
                 }
