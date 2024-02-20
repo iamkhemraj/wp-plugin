7beaeecf2bb8ffbb28f75bc343f34974731cc0b1
khemraj <khemraj@rwit.io>
2024-02-20 12:49:50 +0530

    Fixed taxonomy url path issue

--- wp-content/plugins/my-plugin/libs/helper.php
+++ wp-content/plugins/my-plugin/libs/helper.php
@@ -1,44 +1,67 @@
-<?php
-if (!defined('ABSPATH')) {
-    exit; // Exit if accessed directly
-}
+    <?php
+    if (!defined('ABSPATH')) {
+        exit; // Exit if accessed directly
+    }
 
-function register_post_types($cpt_data) // Register Custom post type
-{
-    if (isset($cpt_data)) {
-        foreach ($cpt_data as $cpt) {
-            if ($cpt->is_activate == true) { // Check if status is active
-                $post_type     = $cpt->post_type;
-                $slug          = $cpt->slug;
-                $tag_data      = $cpt->tag;
-                $term_id       = $cpt->id;
-                $category_data = $cpt->category;
-                
-                $rewrite_slug = array(
-                    'slug' => $slug,
-                    'with_front' => false // This line removes the taxonomy slug from the URL
-                );
-                
-                register_post_type($slug, array(  // Register custom post type
-                    'labels'        => array(
-                        'name'          => $post_type,
-                        'singular_name' => $post_type
-                    ),
-                    'public'      => true,
-                    'has_archive' => true,
-                    'rewrite'     => $rewrite_slug,
-                    'taxonomies'  => array('category','post_tag') // Use the new taxonomy name here
-                ));
+    function register_post_types($cpt_data) {
+        if (isset($cpt_data)) {
+            foreach ($cpt_data as $cpt) {
+                if ($cpt->is_activate == true) {
+                    $post_type = isset($cpt->post_type ) ? $cpt->post_type: '' ;
+                    $slug      = isset($cpt->slug ) ? $cpt->slug: '' ;           
+                    $tag_data  = isset($cpt->tag ) ? $cpt->tag: '' ;             
+                    $term_id   = isset($cpt->id ) ? $cpt->id: '' ;             
+                    $category  = isset($cpt->category )? ucwords($cpt->category) : '' ; 
+                    $tax_slug  = str_replace(' ', '_', strtolower($category)); // Replace spaces with underscores
 
-                if (isset($category_data) && !empty($category_data)) {  // Insert category       
-                    $term = wp_insert_term($category_data, 'new_taxonomy_name', array('term_id' => $term_id));   
-                }
-                
-                if (isset($tag_data) && !empty($tag_data)) { // Insert tags
-                    $term = wp_insert_term($tag_data, 'new_taxonomy_name', array('term_id' => $term_id));     
+                    $rewrite_slug = array(
+                        'slug' => $slug,
+                        'with_front' => false // This line removes the taxonomy slug from the URL
+                    );
+
+                    // Register custom post type
+                    register_post_type($slug, array(
+                        'labels'        => array(
+                            'name'          => $post_type,
+                            'singular_name' => $post_type
+                        ),
+                        'public'      => true,
+                        'has_archive' => true,
+                        'rewrite'     => $rewrite_slug,
+                        'taxonomies'  => array('post_tag') // Use the new taxonomy name here
+                    ));
+
+                    // Register custom taxonomy for categories 
+                    register_taxonomy( $tax_slug , $slug, array(
+                        "hierarchical"      => true,
+                        "labels"            => array(
+                            "name"          => "Categories",
+                            "singular_name" => "Custom Category",
+                        ),
+                        "rewrite"           => array("slug" => $slug), // Rewrite slug  
+                        "public"            => true,
+                        "show_ui"           => true,
+                        "show_in_rest"      => true,
+                        "show_admin_column" => true,
+                        "query_var"         => true,
+                        "capabilities"      => array(
+                            "manage_terms"  => "manage_categories",
+                            "edit_terms"    => "edit_categories",
+                            "delete_terms"  => "delete_categories",
+                            "assign_terms"  => "assign_categories", 
+                        ),
+                        "show_in_nav_menus" => true,
+                    ));
+
+                    if (isset($category) && !empty($category)) {  // Insert category       
+                        $term = wp_insert_term($category,   $tax_slug, array('term_id' => $term_id));   
+                    }
+
+                    if (isset($tag_data) && !empty($tag_data)) { // Insert tags
+                        $term = wp_insert_term($tag_data, 'post_tag', array('term_id' => $term_id));     
+                    }
                 }
             }
         }
     }
-}
-?>
+    ?>
